706efd91be994fa17ee7dbecb7d53a1c72ecb95a
wachira <wachiraa954@gmail.com>
2022-09-07 22:43:52 +0300

    done

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -2,5 +2,30 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add routes
-  
+  get '/messages' do 
+  messages=Message.all
+  messages.to_json
+  end
+  # ApplicationController POST /messages creates a new message in the database
+   post '/messages' do 
+   messages=Message.create(
+     body: params[:body],
+     username: params[:username]
+   )
+   messages.to_json
+   end
+   # ApplicationController PATCH /messages/:id updates the body of the message in the database
+   patch '/messages/:id' do 
+    messages=Message.find(params[:id])
+   messages.update(
+     body: params[:body]
+   )
+   messages.to_json
+   end 
+   # ApplicationController DELETE /messages/:id deletes the message from the database
+  delete '/messages/:id' do 
+   message=Message.find(params[:id])
+   message.destroy
+   message.to_json
+  end
 end
